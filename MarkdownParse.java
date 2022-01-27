7ab3fb24da358c179f0e98c80b1b05461b1efb29
Gavin Roberts <gavinsroberts13@gmail.com>
2022-01-26 19:34:31 -0800

    added tester

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Stack;
 import java.util.HashMap;
 
-
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
