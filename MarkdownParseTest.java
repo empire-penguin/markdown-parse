c12a7f5c210180da95fdfe8b9f6c15002ed9eff5
Gavin Roberts <gavinsroberts13@gmail.com>
2022-02-02 19:43:37 -0800

    sync

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.List;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.io.IOException;
 
 public class MarkdownParseTest {
@@ -11,7 +10,7 @@ public class MarkdownParseTest {
 
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        assertEquals(3, 1 + 1);
     }
 
     @Test
